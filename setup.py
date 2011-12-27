c6f4e09b23aa72d4f56a1b72cf8f73de409c291e
Rafael Oliveira <rafaelbco@gmail.com>
2011-12-27 17:50:33 +0000

    Preparing release 0.6
    git-svn-id: svn://localhost/tmp/svn2git/svn_repo/trunk@36 bb234ac0-767f-4816-ba6b-0a88e8692fb2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.6dev'
+version = '0.6'
 
 def get_console_script_entry_point(function, module):
     return 'nautilus_%(module)s_%(function)s = rbco.nautilusscripts.%(module)s:%(function)s' % {
